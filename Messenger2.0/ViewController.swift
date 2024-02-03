49eb1c7feed84f26716b1aa0735b20c9af99db49
DanHozan123 <danhozan@gmail.com>
2024-02-03 02:03:10 +0200

    Initial Commit

---
+++ Messenger2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger2.0
+//
+//  Created by Dan Hozan on 03.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
