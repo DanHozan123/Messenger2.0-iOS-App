57ee638de749c7a46f2af9fb33933db9bef28f33
DanHozan123 <danhozan@gmail.com>
2024-02-06 14:51:13 +0200

    Firebase login and registration

--- Messenger2.0/FirebaseHelpers/FirebaseUserListener.swift
+++ Messenger2.0/FirebaseHelpers/FirebaseUserListener.swift
@@ -16,8 +16,22 @@ class FirebaseUserListener {
     
     //MARK: - Login
     
+    func loginUserWithEmail(email: String, password: String, completion: @escaping (_ error: Error?, _ isEmailVerified: Bool) -> Void) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authDataResult, error) in
+            
+            if error == nil && authDataResult!.user.isEmailVerified {
+                //FirebaseUserListener.shared.downloadUserFromFirebase(userId: authDataResult!.user.uid, email: email)
+                completion(error, true)
+            } else {
+                print("email is not verified")
+                completion(error, false)
+            }
+        }
+    }
+    
     //MARK: - Register
-    func registerUserWith(email: String, password: String, complition: @escaping( _ error: Error?) -> Void){
+    func registerUserWith(email: String, password: String, complition: @escaping(_ error: Error?) -> Void){
         Auth.auth().createUser(withEmail: email, password: password) { ( authDataResult, error) in
             print()
             complition(error)
@@ -47,6 +61,35 @@ class FirebaseUserListener {
             print(error.localizedDescription, " adding user")
         }
     }
+    
+    //MARK: - Download
+    
+    func downloadUserFromFirebase(userId: String, email: String? = nil) {
+        
+        FirebaseReference(collectionReferance: .User).document(userId).getDocument { (querySnapshot, error) in
+            
+            guard let document = querySnapshot else {
+                print("no document for user")
+                return
+            }
+            
+            let result = Result {
+                try? document.data(as: User.self)
+            }
+            
+            switch result {
+            case .success(let userObject):
+                if let user = userObject {
+                    saveUserLocally(user: user)
+                } else {
+                    print(" Document does not exist")
+                }
+            case .failure(let error):
+                print("Error decoding user ", error)
+            }
+        }
+    }
+    
     
     
 }
